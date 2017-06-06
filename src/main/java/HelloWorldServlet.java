f1dd4d50d73790f3450c2c642153ad212b6972e0
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-06 11:09:38 -0500

    Finished the exercise for hello world with tomcat server

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,11 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "SomeServlet", urlPatterns = "/some/path")
+@WebServlet(name = "HelloWorldServlet")
 public class HelloWorldServlet extends HttpServlet {
+    private int hitCount;
+    public void init()
+    {
+        // Reset hit counter.
+        hitCount = 0;
+    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        String name = request.getParameter("name");
+        hitCount++;
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h1 align=\"center\">Hello, " + name + "!</h1>\n" + "<h3 align=\"center\">Hit Counter</h3>\n" + "<h2 align=\"center\">" + hitCount + "</h2>\n");
     }
 }
